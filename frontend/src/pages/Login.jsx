9974a3a967481ecee4d677fae71e126d45b6f3e0
Iulian Gherman <IulianGherman@hotmail.com>
2022-04-08 11:12:34 +0100

    Register new user functionality

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { FaSignInAlt } from "react-icons/fa";
-import {toast} from 'react-toastify';
+import { toast } from "react-toastify";
+import { useSelector, useDispatch } from "react-redux";
+import { login } from "../features/auth/authSlice";
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -10,40 +12,68 @@ function Login() {
 
   const { email, password } = formData;
 
-  const onChange = (e) =>{
-      setFormData((prevState) => ({
-          ...prevState,
-          [e.target.name]: e.target.value,
-      }))
-  }
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
   const onSubmit = (e) => {
-      e.preventDefault()
+    e.preventDefault();
 
-  }
+    const userData = {
+      email,
+      password,
+    };
+
+    dispatch(login(userData));
+  };
 
   return (
     <>
       <section className="heading">
         <h1>
-            <FaSignInAlt /> Login
+          <FaSignInAlt /> Login
         </h1>
         <p>Please login to get support</p>
       </section>
 
       <section className="form">
         <form onSubmit={onSubmit}>
-            <div className="form-group">
-                <input type="email" className="form-control" id="email" value={email} name="email" placeholder="Enter your email" required onChange={onChange} />
-            </div>
-            <div className="form-group">
-                <input type="password" className="form-control" id="password" value={password} name="password" placeholder="Enter password" required onChange={onChange} />
-            </div>
-            <div className="form-group">
-                <button className="btn btn-block">
-                    Submit
-                </button>
-            </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              value={email}
+              name="email"
+              placeholder="Enter your email"
+              required
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              value={password}
+              name="password"
+              placeholder="Enter password"
+              required
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block">Submit</button>
+          </div>
         </form>
       </section>
     </>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { FaUser } from "react-icons/fa";
-import {toast} from 'react-toastify';
+import { toast } from "react-toastify";
+import { useSelector, useDispatch } from "react-redux";
+import { register, reset } from "../features/auth/authSlice";
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -12,49 +15,110 @@ function Register() {
 
   const { name, email, password, password2 } = formData;
 
-  const onChange = (e) =>{
-      setFormData((prevState) => ({
-          ...prevState,
-          [e.target.name]: e.target.value,
-      }))
-  }
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    // After login redirect
+    if (isSuccess || user) {
+      navigate("/");
+    }
+    dispatch(reset());
+  }, [isError, isSuccess, user, message, navigate, dispatch]);
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
   const onSubmit = (e) => {
-      e.preventDefault()
+    e.preventDefault();
 
-      if(password !== password2) {
-          toast.error('Passwords do not match!')
-      }
-  }
+    if (password !== password2) {
+      toast.error("Passwords do not match!");
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+      };
+      dispatch(register(userData));
+    }
+  };
 
   return (
     <>
       <section className="heading">
         <h1>
-            <FaUser /> Register
+          <FaUser /> Register
         </h1>
         <p>Please create an account</p>
       </section>
 
       <section className="form">
         <form onSubmit={onSubmit}>
-            <div className="form-group">
-                <input type="text" className="form-control" id="name" value={name} name="name" placeholder="Enter your name" required onChange={onChange} />
-            </div>
-            <div className="form-group">
-                <input type="email" className="form-control" id="email" value={email} name="email" placeholder="Enter your email" required onChange={onChange} />
-            </div>
-            <div className="form-group">
-                <input type="password" className="form-control" id="password" value={password} name="password" placeholder="Enter password" required onChange={onChange} />
-            </div>
-            <div className="form-group">
-                <input type="password" className="form-control" id="password2" value={password2} name="password2" placeholder="Confirm password" required onChange={onChange} />
-            </div>
-            <div className="form-group">
-                <button className="btn btn-block">
-                    Submit
-                </button>
-            </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              value={name}
+              name="name"
+              placeholder="Enter your name"
+              required
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              value={email}
+              name="email"
+              placeholder="Enter your email"
+              required
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              value={password}
+              name="password"
+              placeholder="Enter password"
+              required
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password2"
+              value={password2}
+              name="password2"
+              placeholder="Confirm password"
+              required
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block">Submit</button>
+          </div>
         </form>
       </section>
     </>
